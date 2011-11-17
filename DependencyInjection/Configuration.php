3936b8c73ff7878eeeb946d6d4ea85fd89fe2a4f
Benjamin Eberlei <kontakt@beberlei.de>
2011-11-18 00:01:32 +0100

    Prepare move to Doctrine organization

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Symfony\Bundle\DoctrineMigrationsBundle\DependencyInjection;
+namespace Doctrine\Bundle\DoctrineMigrationsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
--- DoctrineMigrationsBundle.php
+++ DoctrineMigrationsBundle.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Bundle\DoctrineMigrationsBundle;
+namespace Doctrine\Bundle\DoctrineMigrationsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
