fe13288dc2c7bd878f041117198fd3974f391a11
mike <mike.simonson@gmail.com>
2017-10-31 12:40:50 +0100

    Removing the copyright header

--- Command/Helper/DoctrineCommandHelper.php
+++ Command/Helper/DoctrineCommandHelper.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
- * This file is part of the Doctrine MigrationsBundle
- *
- * The code was originally distributed inside the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- * (c) Doctrine Project, Guilehrme Blanco <guilhermeblanco@hotmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Doctrine\Bundle\MigrationsBundle\Command\Helper;
 
 use Doctrine\Bundle\DoctrineBundle\Command\Proxy\DoctrineCommandHelper as BaseDoctrineCommandHelper;
