2a72becf18fb28341b24cb041fe8454217ca3e77
Christophe Coevoet <stof@notk.org>
2018-12-03 12:55:33 +0100

    Merge pull request #245 from chalasr/sf-42-deprec
    Fix deprecation on symfony/config 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,6 +10,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use function constant;
 use function in_array;
 use function is_string;
+use function method_exists;
 use function strlen;
 use function strpos;
 use function strtoupper;
@@ -27,8 +28,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder() : TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('doctrine_migrations', 'array');
+        $treeBuilder = new TreeBuilder('doctrine_migrations');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('doctrine_migrations', 'array');
+        }
 
         $organizeMigrationModes = $this->getOrganizeMigrationsModes();
 
