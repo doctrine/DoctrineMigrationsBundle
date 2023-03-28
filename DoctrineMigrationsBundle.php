4acae5c68518569debfbe872cbcbeb0c5afeac89
Bernard van der Esch <b.vanderesch@gmail.com>
2023-03-28 12:32:09 +0200

    hide deprecation warning

--- DoctrineMigrationsBundle.php
+++ DoctrineMigrationsBundle.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class DoctrineMigrationsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
       $container->addCompilerPass(new ConfigureDependencyFactoryPass());
     }
