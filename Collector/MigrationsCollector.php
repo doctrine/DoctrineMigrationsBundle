9f83e13516cbf1be893703c6d2b3ed22b48653cf
Grégoire Paris <postmaster@greg0ire.fr>
2024-03-26 19:55:16 +0100

    Cover all directories with our tooling
    The goal is to hold new code with higher standards, and to have the same
    configuration on all branches. Since I have no proof addressing static
    analysis issues fixes any actual bugs, let us ignore them all on this
    branch.

--- Collector/MigrationsCollector.php
+++ Collector/MigrationsCollector.php
@@ -10,6 +10,11 @@ use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\VarDumper\Cloner\Data;
+use Throwable;
+
+use function count;
+use function get_class;
 
 class MigrationsCollector extends DataCollector
 {
@@ -21,20 +26,18 @@ class MigrationsCollector extends DataCollector
     public function __construct(DependencyFactory $dependencyFactory, MigrationsFlattener $migrationsFlattener)
     {
         $this->dependencyFactory = $dependencyFactory;
-        $this->flattener = $migrationsFlattener;
+        $this->flattener         = $migrationsFlattener;
     }
 
-    /**
-     * @return void
-     */
-    public function collect(Request $request, Response $response, \Throwable $exception = null)
+    /** @return void */
+    public function collect(Request $request, Response $response, ?Throwable $exception = null)
     {
-        if (!empty($this->data)) {
+        if (! empty($this->data)) {
             return;
         }
 
         $metadataStorage = $this->dependencyFactory->getMetadataStorage();
-        $planCalculator = $this->dependencyFactory->getMigrationPlanCalculator();
+        $planCalculator  = $this->dependencyFactory->getMigrationPlanCalculator();
 
         try {
             $executedMigrations = $metadataStorage->getExecutedMigrations();
@@ -49,45 +52,42 @@ class MigrationsCollector extends DataCollector
 
         $availableMigrations = $planCalculator->getMigrations();
 
-        $this->data['available_migrations_count'] = count($availableMigrations);
-        $unavailableMigrations = $executedMigrations->unavailableSubset($availableMigrations);
+        $this->data['available_migrations_count']   = count($availableMigrations);
+        $unavailableMigrations                      = $executedMigrations->unavailableSubset($availableMigrations);
         $this->data['unavailable_migrations_count'] = count($unavailableMigrations);
 
-        $newMigrations = $availableMigrations->newSubset($executedMigrations);
-        $this->data['new_migrations'] = $this->flattener->flattenAvailableMigrations($newMigrations);
+        $newMigrations                     = $availableMigrations->newSubset($executedMigrations);
+        $this->data['new_migrations']      = $this->flattener->flattenAvailableMigrations($newMigrations);
         $this->data['executed_migrations'] = $this->flattener->flattenExecutedMigrations($executedMigrations, $availableMigrations);
 
         $this->data['storage'] = get_class($metadataStorage);
-        $configuration = $this->dependencyFactory->getConfiguration();
-        $storage = $configuration->getMetadataStorageConfiguration();
+        $configuration         = $this->dependencyFactory->getConfiguration();
+        $storage               = $configuration->getMetadataStorageConfiguration();
         if ($storage instanceof TableMetadataStorageConfiguration) {
-            $this->data['table'] = $storage->getTableName();
+            $this->data['table']  = $storage->getTableName();
             $this->data['column'] = $storage->getVersionColumnName();
         }
 
-        $connection = $this->dependencyFactory->getConnection();
+        $connection           = $this->dependencyFactory->getConnection();
         $this->data['driver'] = get_class($connection->getDriver());
-        $this->data['name'] = $connection->getDatabase();
+        $this->data['name']   = $connection->getDatabase();
 
         $this->data['namespaces'] = $configuration->getMigrationDirectories();
     }
 
-    /**
-     * @return string
-     */
+    /** @return string */
     public function getName()
     {
         return 'doctrine_migrations';
     }
 
+    /** @return array<string, mixed>|Data */
     public function getData()
     {
         return $this->data;
     }
 
-    /**
-     * @return void
-     */
+    /** @return void */
     public function reset()
     {
         $this->data = [];
