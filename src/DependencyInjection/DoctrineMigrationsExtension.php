27b05cc5f6f0b14f07140885aa2fd67364e8dc69
Grégoire Paris <postmaster@greg0ire.fr>
2024-03-27 08:34:04 +0100

    Address static analysis issues from the baselines

--- src/DependencyInjection/DoctrineMigrationsExtension.php
+++ src/DependencyInjection/DoctrineMigrationsExtension.php
@@ -38,8 +38,7 @@ class DoctrineMigrationsExtension extends Extension
     /**
      * Responds to the migrations configuration parameter.
      *
-     * @param mixed[][] $configs
-     * @psalm-param array<string, array<string, array<string, array<string, string>|string>|string>> $configs
+     * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
