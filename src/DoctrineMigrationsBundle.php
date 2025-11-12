a03b6a39e74423f62907b8d3ae2f9fe77ad4395e
HypeMC <hypemc@gmail.com>
2025-11-12 22:21:29 +0100

    Add service migrations

--- src/DoctrineMigrationsBundle.php
+++ src/DoctrineMigrationsBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\Bundle\MigrationsBundle;
 
 use Doctrine\Bundle\MigrationsBundle\DependencyInjection\CompilerPass\ConfigureDependencyFactoryPass;
+use Doctrine\Bundle\MigrationsBundle\DependencyInjection\CompilerPass\RegisterMigrationsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,6 +17,7 @@ class DoctrineMigrationsBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new ConfigureDependencyFactoryPass());
+        $container->addCompilerPass(new RegisterMigrationsPass());
     }
 
     public function getPath(): string
